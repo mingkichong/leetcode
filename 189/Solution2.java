b167920db054c67f78bae12e32cd3fb6cf968e94
Ming Ki Chong <unexpected.exception@gmail.com>
2018-06-03 00:58:20 +0200

    https://leetcode.com/problems/rotate-array/discuss/54250/Easy-to-read-Java-solution/142238

---
+++ 189/Solution2.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+public class Solution2 {
+    public void rotate(int[] nums, int k) {
+        int N = nums.length;
+        k = (k % N);
+        // k = nums.length-k; //left or right rotate
+        reverse(nums, 0, N-1);
+        reverse(nums, 0, k-1);
+        reverse(nums, k, N-1);
+
+    }
+
+    public void reverse(int[] nums, int l, int r){
+        while(l < r){
+            int temp = nums[l];
+            nums[l] = nums[r];
+            nums[r] = temp;
+            l++;r--;
+        }
+    }
+
+    public static void main(String args[]){
+    	Solution2 s = new Solution2();
+    	int[] nums = {1,2,3,4,5,6};
+    	int k = 1;
+        System.out.println("k=" + k);
+    	System.out.println(Arrays.toString(nums));
+    	s.rotate(nums, k);
+    	System.out.println(Arrays.toString(nums));
+    }   
+}
