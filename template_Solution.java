679966633d5c2212cf6efdd10aedeef401032fda
Ming Ki Chong <unexpected.exception@gmail.com>
2018-06-09 20:15:30 +0200

    template

---
+++ template_Solution.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Solution {
+    public void __something() {
+        
+    }
+
+    public static void main(String args[]){
+    	Solution s = new Solution();
+    	System.out.println(s.__something());
+    }
+}
