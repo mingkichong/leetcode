2a13a9f04796e66edd77cfc2dbe01a36dea94c41
Ming Ki Chong <unexpected.exception@gmail.com>
2018-06-16 19:54:08 +0200

    https://leetcode.com/problems/palindrome-number

---
+++ 9/Solution.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x < 0){ return false;}
+        if(x < 9){ return true; }
+        if(x ==0){ return true; }
+        int originalX = x;
+        int reverse = 0;
+        while(x > 0){
+        	reverse *= 10;
+        	int d = x % 10;
+        	x /= 10;
+        	reverse += d;
+        }
+        System.out.println(reverse);
+        return reverse == originalX;
+    }
+
+    public static void main(String args[]){
+    	Solution s = new Solution();
+    	System.out.println(s.isPalindrome(12321));
+    }
+}
