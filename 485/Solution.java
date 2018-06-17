7a559d9b77c374d425604591ae3f12ec7c718b40
Ming Ki Chong <unexpected.exception@gmail.com>
2018-06-17 13:51:17 +0200

    https://leetcode.com/problems/max-consecutive-ones

---
+++ 485/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int count = 0;
+        int max = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] == 0){
+                count = 0;
+            }
+            else{
+                count++;
+                max = Math.max(max, count);
+            }
+        }
+        return max;
+    }
+}
