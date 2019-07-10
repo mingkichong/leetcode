563c86c999e60b0cfbb679dfe7a81eee32b8b14d
Ming Ki Chong <unexpected.exception@gmail.com>
2019-07-10 10:42:34 -0700

    changed solution to an algorithmic function

--- 1108/Solution.java
+++ 1108/Solution.java
@@ -2,11 +2,21 @@ import java.util.*;
 
 class Solution {
     public String defangIPaddr(String address) {
-        return address.replace(".", "[.]");
+        StringBuilder sb = new StringBuilder();
+        for(char a : address.toCharArray()){
+            if(a == '.'){
+                sb.append("[.]");
+            }
+            else{
+                sb.append(a);
+            }
+        }
+        return sb.toString();
+        // return address.replace(".", "[.]");
     }
 
     public static void main(String args[]){
-    	Solution s = new Solution();
-    	System.out.println(s.defangIPaddr("1.1.1.1"));
+        Solution s = new Solution();
+        System.out.println(s.defangIPaddr("1.1.1.1"));
     }
 }
