0d0e476ebc68b0af379e9c37fd0cebc6cc8d02a7
Ming Ki Chong <unexpected.exception@gmail.com>
2019-07-10 18:06:40 -0700

    Changed tabs to spaces in template

--- template_Solution.java
+++ template_Solution.java
@@ -6,7 +6,7 @@ class Solution {
     }
 
     public static void main(String args[]){
-    	Solution s = new Solution();
-    	System.out.println(s.__something());
+        Solution s = new Solution();
+        System.out.println(s.__something());
     }
 }
