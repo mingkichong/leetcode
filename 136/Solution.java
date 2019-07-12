d33856ed6b7c62620b8fa2ced197b5ceba43faaa
Ming Ki Chong <unexpected.exception@gmail.com>
2019-07-12 00:26:01 -0700

    https://leetcode.com/problems/single-number/

---
+++ 136/Solution.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        int n = nums[0];
+        if(nums.length > 1){
+            for(int i = 1; i < nums.length; i++){
+                n ^= nums[i];
+            }
+        }
+        return n;
+    }
+
+    public static void main(String args[]){
+        Solution s = new Solution();
+        int [] nums = {4,1,2,1,2,4,9,6,2,2,6};
+        System.out.println(s.singleNumber(nums));
+    }
+}
