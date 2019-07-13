929416c107c4adc79735b640776c442d6deba654
Ming Ki Chong <unexpected.exception@gmail.com>
2019-07-12 22:48:45 -0700

    check if directory already exists

--- make_new_solution.sh
+++ make_new_solution.sh
@@ -1,9 +1,14 @@
 #!/usr/local/bin/bash
 
 start_making(){
-	mkdir $1;
-	cp template_Solution.java ./$1/
-	mv ./$1/template_Solution.java ./$1/Solution.java
+    ### Check if a directory does not exist ###
+    if [ ! -d "./$1" ]; then
+        mkdir $1;
+        cp template_Solution.java ./$1/
+        mv ./$1/template_Solution.java ./$1/Solution.java
+    else
+        echo "directory ./$1/ already exists"
+    fi
 }
 
 if [ "$1" != "" ]; then
