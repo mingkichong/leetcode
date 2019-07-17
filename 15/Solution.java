3def2c31fc062546c7fea50f82aa966bc510dfcd
Ming Ki Chong <unexpected.exception@gmail.com>
2019-07-16 19:59:01 -0700

    https://leetcode.com/problems/3sum/ (using set and map without sorting)

--- 15/Solution.java
+++ 15/Solution.java
@@ -2,36 +2,60 @@ import java.util.*;
 
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> answers = new ArrayList<List<Integer>>();
-        if(nums.length < 3){
-            return answers;
-        }
 
-        Arrays.sort(nums);
-        HashSet<List<Integer>> set = new HashSet<List<Integer>>();
-        for(int i = 0; i < nums.length-2; i++){
-            int r = nums.length - 1;
-            int l = i + 1;
-            while(l < r){
-                if(nums[i] + nums[l] + nums[r] == 0){
-                    List<Integer> answer = new ArrayList<Integer>();
-                    answer.add(nums[i]);
-                    answer.add(nums[l]);
-                    answer.add(nums[r]);
-                    if(!set.contains(answer)){
-                        set.add(answer);
-                        answers.add(answer);
+        // Arrays.sort(nums);
+        Set<List<Integer>> set = new HashSet<List<Integer>>();
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for(int n : nums){
+            map.put(n, map.getOrDefault(n, 0) + 1);
+        }
+        for(int i = 0 ; i < nums.length-1; i++){
+            for(int j = i + 1; j < nums.length; j++){
+                int target = -1 * (nums[i] + nums[j]);
+                if(map.containsKey(target)){
+                    int targetCount = 0;
+                    targetCount += (nums[i] == target) ? 1 : 0;
+                    targetCount += (nums[j] == target) ? 1 : 0;
+                    if(targetCount < map.get(target)){
+                        Integer [] sum = new Integer[]{nums[i], nums[j], target};
+                        Arrays.sort(sum);
+                        set.add(Arrays.asList(sum));
                     }
-                    int prevR = nums[r];while(r > 0 && prevR == nums[r]){r--;}
-                    int prevL = nums[l];while(l < nums.length && prevL == nums[l]){l++;}
-                }else if(nums[r] + nums[l] + nums[i] > 0){
-                    int prevR = nums[r];while(r > 0 && prevR == nums[r]){r--;}
-                }
-                else{
-                    int prevL = nums[l];while(l < nums.length && prevL == nums[l]){l++;}
                 }
             }
         }
+        return new ArrayList<>(set);
+
+        // List<List<Integer>> answers = new ArrayList<List<Integer>>();
+        // if(nums.length < 3){
+        //     return answers;
+        // }
+
+        // Arrays.sort(nums);
+        // HashSet<List<Integer>> set = new HashSet<List<Integer>>();
+        // for(int i = 0; i < nums.length-2; i++){
+        //     int r = nums.length - 1;
+        //     int l = i + 1;
+        //     while(l < r){
+        //         if(nums[i] + nums[l] + nums[r] == 0){
+        //             List<Integer> answer = new ArrayList<Integer>();
+        //             answer.add(nums[i]);
+        //             answer.add(nums[l]);
+        //             answer.add(nums[r]);
+        //             if(!set.contains(answer)){
+        //                 set.add(answer);
+        //                 answers.add(answer);
+        //             }
+        //             int prevR = nums[r];while(r > 0 && prevR == nums[r]){r--;}
+        //             int prevL = nums[l];while(l < nums.length && prevL == nums[l]){l++;}
+        //         }else if(nums[r] + nums[l] + nums[i] > 0){
+        //             int prevR = nums[r];while(r > 0 && prevR == nums[r]){r--;}
+        //         }
+        //         else{
+        //             int prevL = nums[l];while(l < nums.length && prevL == nums[l]){l++;}
+        //         }
+        //     }
+        // }
 
         // for(int i = 0; i < nums.length-2; i++){
         //     for(int j = i+1; j < nums.length-1; j++){
@@ -50,12 +74,12 @@ class Solution {
         //         }
         //     }
         // }
-        return answers;
+        // return answers;
     }
 
     public static void main(String args[]){
         Solution s = new Solution();
-        int[] nums = new int[]{-1,0,1,2};
+        int[] nums = new int[]{-1, 0, 1, 2, -1, -4};
         System.out.println(Arrays.deepToString(s.threeSum(nums).toArray()));
     }
 }
