82c738cb13b91ba11ec972e972ebe0c091bb1454
Ming Ki Chong <unexpected.exception@gmail.com>
2019-07-21 17:20:43 -0700

    https://leetcode.com/problems/single-number/submissions/ (removed redundant if statement)

--- 136/Solution.java
+++ 136/Solution.java
@@ -3,10 +3,8 @@ import java.util.*;
 class Solution {
     public int singleNumber(int[] nums) {
         int n = nums[0];
-        if(nums.length > 1){
-            for(int i = 1; i < nums.length; i++){
-                n ^= nums[i];
-            }
+        for(int i = 1; i < nums.length; i++){
+            n ^= nums[i];
         }
         return n;
     }
