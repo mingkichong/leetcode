37c7df5010718c42e723481ce4097a9d5eebb460
Ming Ki Chong <unexpected.exception@gmail.com>
2019-08-12 04:35:36 -0700

    Move template classes to Templates/

--- make_new_solution.sh
+++ make_new_solution.sh
@@ -4,9 +4,9 @@ start_making(){
     ### Check if a directory does not exist ###
     if [ ! -d "./$1" ]; then
         mkdir $1;
-        cp template_Solution.java ./$1/
+        cp Templates/template_Solution.java ./$1/
         mv ./$1/template_Solution.java ./$1/Solution.java
-        echo "java Solution" > ./$1/run.sh
+        echo "javac *.java && java Solution" > ./$1/run.sh
         chmod 555 ./$1/run.sh
     else
         echo "directory ./$1/ already exists"
