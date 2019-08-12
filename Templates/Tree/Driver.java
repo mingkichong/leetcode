9474a1a34535e18e0fb84a844f7d69f9679f49c1
Ming Ki Chong <unexpected.exception@gmail.com>
2019-08-12 06:08:00 -0700

    Move TreeNode classes to Tree/

---
+++ Templates/Tree/Driver.java
@@ -0,0 +1,7 @@
+class Driver{
+    public static void main(String args[]){
+        int [][] nums = {{10},{5},{15},{3},{7},{13},{18},{1},null,{6}};
+        TreeNode head = TreeNode.createList(nums);
+        TreeNode.printWithOrder(head, TraverseOrder.PREORDER);
+    }
+}
---
+++ Templates/Tree/TreeNode.java
@@ -0,0 +1,81 @@
+import java.util.*;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+
+    public static TreeNode createList(int[][] nums){
+        if(nums == null || nums.length == 0){
+            return null;
+        }
+        if(nums.length == 1 && nums[0] == null){
+            return null;
+        }
+        TreeNode [] nodes = new TreeNode[nums.length];
+        for(int i = 0; i < nodes.length; i++){
+            if(nums[i] == null){
+                nodes[i] = null;
+            }
+            else{
+                nodes[i] = new TreeNode(nums[i][0]);
+            }
+        }
+        int index = 0;
+        ArrayList<TreeNode> queue = new ArrayList<TreeNode>();
+        queue.add(nodes[index++]);
+        while(queue.size() > 0){
+            TreeNode node = queue.remove(0);
+            if(index < nodes.length){
+                node.left = nodes[index++];
+                queue.add(node.left);
+            }
+            if(index < nodes.length){
+                node.right = nodes[index++];
+                queue.add(node.right);
+            }
+        }
+        return nodes[0];
+    }
+
+    public static void main(String args[]){
+        int [][] nums = {{10},{5},{15},{3},{7},{13},{18},{1},null,{6}};
+        TreeNode node = createList(nums);
+        printWithOrder(node, TraverseOrder.PREORDER);
+        printWithOrder(node, TraverseOrder.INORDER);
+        printWithOrder(node, TraverseOrder.POSTORDER);
+    }
+
+    public static void printWithOrder(TreeNode node, TraverseOrder order){
+        System.out.print("[");
+        traversePrint(node, order);
+        System.out.println("]");
+    }
+
+    static void traversePrint(TreeNode node, TraverseOrder order){
+        if(node == null){
+            return;
+        }
+        switch(order){
+            case PREORDER:
+                System.out.print(node.val);
+                System.out.print(", ");
+                traversePrint(node.left, order);
+                traversePrint(node.right, order);
+                break;
+            case INORDER:
+                traversePrint(node.left, order);
+                System.out.print(node.val);
+                System.out.print(", ");
+                traversePrint(node.right, order);
+                break;
+            case POSTORDER:
+                traversePrint(node.left, order);
+                traversePrint(node.right, order);
+                System.out.print(node.val);
+                System.out.print(", ");
+                break;
+        }
+    }
+}
