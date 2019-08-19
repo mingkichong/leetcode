27c16e05dcdf5352892d44d6d68838b1391cabd2
Ming Ki Chong <unexpected.exception@gmail.com>
2019-08-18 23:07:07 -0700

    Move TreeNode main to Driver main

--- Templates/Tree/Driver.java
+++ Templates/Tree/Driver.java
@@ -3,6 +3,8 @@ class Driver{
         int [][] nums = {{10},{5},{15},{3},{7},{13},{18},{1},null,{6}};
         TreeNode head = TreeNode.createList(nums);
         TreeNode.printWithOrder(head, TraverseOrder.PREORDER);
+        TreeNode.printWithOrder(head, TraverseOrder.INORDER);
+        TreeNode.printWithOrder(head, TraverseOrder.POSTORDER);
         TreeNode.printBFS(head);
     }
 }
