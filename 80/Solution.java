d53f2df9a942e337df5794bba46fcb9ff0581591
Ming Ki Chong <unexpected.exception@gmail.com>
2019-08-18 21:17:57 -0700

    https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/ (time O(n), space O(1))

--- 80/Solution.java
+++ 80/Solution.java
@@ -1,49 +1,38 @@
 import java.util.*;
 
 class Solution {
-/// time O(n^2) space O(1)
+/// time O(n) space O(1)
     public int removeDuplicates(int[] nums) {
         final int NUM_DUPLICATES = 2;
-        if(nums == null){
+        if(nums == null || nums.length == 0){
             return 0;
         }
         final int N = nums.length;
-        if(N == 0 || N == 1 || N == 2){
-            return N;
-        }
-
         int slow = 0, fast = 1;
-        int nDup = 0; int length = N;
-        while(fast < N && slow < length){
-            while(fast < length && nums[slow] == nums[fast]){
+        int nDup = 0;
+        while(fast < N && slow < N){
+            while(fast < N && nums[slow] == nums[fast]){
                 if(nDup < NUM_DUPLICATES - 1){
-                    fast++;
                     slow++;
+                    nums[slow] = nums[slow-1];
                     nDup++;
                 }
-                else{
-                    shiftLeft(nums, fast, length);
-                    length--;
-                }
+                fast++;
+            }
+            if(fast < N && nums[slow] != nums[fast]){
+                slow++;
+                nums[slow] = nums[fast];
             }
             nDup = 0;
             fast++;
-            slow++;
         }
-        return length;
-    }
-
-    void shiftLeft(int[] nums, int index, int length){
-        int temp = nums[index];
-        for(int i = index; i < length - 1; i++){
-            nums[i] = nums[i+1];
-        }
-        nums[length - 1] = temp;
+        return slow + 1;
     }
 
     public static void main(String args[]){
         Solution s = new Solution();
         Random random = new Random();
+        // int[]A = {1,2,2};
         int [] A = new int[random.nextInt(40) + 20];
         for(int i = 0; i < A.length; i++){
             A[i] = random.nextInt(10);
