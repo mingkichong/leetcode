579df6af9bf721a542ad131c491a81f4dded039f
Ming Ki Chong <unexpected.exception@gmail.com>
2019-08-28 19:55:43 -0700

    https://leetcode.com/problems/first-missing-positive/ (using hashset)

--- 41/Solution.java
+++ 41/Solution.java
@@ -1,40 +1,21 @@
 import java.util.*;
 
-//https://leetcode.com/problems/first-missing-positive/
-
 class Solution {
     public int firstMissingPositive(int[] nums) {
-        if(isAllZeroNegative(nums) || findSmallestPositiveInt(nums) > 1){
-            return 1;
+        HashSet<Integer> set = new HashSet<>();
+        for(int i = 0; i < nums.length; i++){
+            set.add(nums[i]);
         }
-
-
-        return -1;
-    }
-
-    boolean isAllZeroNegative(int[] nums){
-        for(int n : nums){
-            if(n > 0){
-                return false;
+        for(int n = 1; ;n++){
+            if(!set.contains(n)){
+                return n;
             }
         }
-        return true;
-    }
-    int findSmallestPositiveInt(int[] nums){
-        boolean hasFound = false;
-        int ans = Integer.MAX_VALUE;
-        for(int n : nums){
-            if(n > 0 && ans > n){
-                ans = n;
-                hasFound = true;
-            }
-        }
-        return hasFound ? ans : -1;
     }
 
     public static void main(String args[]){
         Solution s = new Solution();
-        int [] nums = {7,8,9,11,12};
+        int [] nums = {1,2,0,4,6,3};
         System.out.println(s.firstMissingPositive(nums));
     }
 }
