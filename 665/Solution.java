15d4f2a1704398d2ed61713251baa1a0e6825f10
Ming Ki Chong <unexpected.exception@gmail.com>
2019-08-28 20:46:32 -0700

    https://leetcode.com/problems/non-decreasing-array/

--- 665/Solution.java
+++ 665/Solution.java
@@ -3,20 +3,33 @@ import java.util.*;
 class Solution {
 
     public boolean checkPossibility(int[] nums) {
-        if(nums.length <= 2){
+        final int N = nums.length;
+        if(N <= 2){
             return true;
         }
         int dipCount = 0;
-        for(int i = 1; i < nums.length - 1; i++){
-            // if(){
-            //     dipCount++;
-            // }
-            // else if(){
-            //     dipCount++;
-            // }
-            // else if(){
-            //     return false;
-            // }
+        for(int i = 0; i < N - 2; i++){
+            if(nums[i] <= nums[i+1] && nums[i+1] <= nums[i+2]){
+            }
+            else if(nums[i] <= nums[i+2]){
+                if(nums[i+1] > nums[i+2] || nums[i+1] < nums[i]){
+                    dipCount++;
+                    nums[i+1] = nums[i];
+                }
+            }
+            else if(nums[i] > nums[i+2]){
+                if(nums[i] > nums[i+1] && nums[i+1] > nums[i+2]){
+                    return false;
+                }
+                else if(nums[i] > nums[i+1] && nums[i+1] < nums[i+2]){
+                    dipCount++;
+                    nums[i] = nums[i+1];
+                }
+                else if(nums[i] <= nums[i+1]){
+                    dipCount++;
+                    nums[i+2] = nums[i+1];
+                }
+            }
         }
         if(dipCount > 1){
             return false;
@@ -27,7 +40,7 @@ class Solution {
     public static void main(String args[]){
         Solution s = new Solution();
         Random random = new Random();
-        int [] nums = {0,Integer.MAX_VALUE,1,6};
+        int [] nums = {0,Integer.MAX_VALUE,Integer.MAX_VALUE,1,5,7,8,9,10};
         System.out.println(s.checkPossibility(nums));
     }
 }
