1b2503080af458a37a3b09882a20820f4f0e7246
Ming Ki Chong <unexpected.exception@gmail.com>
2019-09-24 03:19:16 -0700

    https://leetcode.com/problems/ugly-number-ii/ (skeleton)

---
+++ 264/Solution.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    final static boolean DEBUG = true;
+    final static boolean RANDOM_INPUT = true;
+
+    public int nthUglyNumber(int n) {
+        return -1;
+    }
+
+    public static void main(String args[]){
+        Solution s = new Solution();
+        System.out.println(s.nthUglyNumber(1690));
+    }
+}
+
