e317c43ade3aa20a20f6a96964f6c50f5ca4c2b1
Ming Ki Chong <unexpected.exception@gmail.com>
2019-09-25 01:37:56 -0700

    https://leetcode.com/problems/remove-linked-list-elements/

---
+++ 203/Solution.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        if(head == null){
+            return null;
+        }
+        ListNode node = head;
+        ListNode prev = new ListNode(-1);
+        ListNode first = prev;
+        prev.next = head;
+        node = head;
+        while(node != null){
+            while(node.val == val){
+                prev.next = node.next;
+                node = node.next;
+                if(node == null){
+                    break;
+                }
+            }
+            prev = node;
+            if(node != null){
+                node = node.next;
+            }
+            else{
+                break;
+            }
+        }
+        return first.next;
+    }
+
+    public static void main(String args[]){
+        Solution s = new Solution();
+        ListNode head = ListNode.createList(new int[]{1,1,1,1,1,1,11,1,1,1,1,1});
+        System.out.println(s.removeElements(head, 1));
+    }
+}
+
