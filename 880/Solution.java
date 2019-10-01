dd9dea044803eec5fbdb887d990ca8de25e5dd88
Ming Ki Chong <unexpected.exception@gmail.com>
2019-10-01 01:38:29 -0700

    https://leetcode.com/problems/decoded-string-at-index/ (by counting length)

--- 880/Solution.java
+++ 880/Solution.java
@@ -4,32 +4,33 @@ class Solution {
     final static boolean DEBUG = true;
     final static boolean RANDOM_INPUT = true;
 
-    public String decodeAtIndex(String S, int K) {
+    public String decodeAtIndex(String S, int k) {
         if(S == null || S.length() == 0){
             return null;
         }
-        String buffer = "";
+        long decodedLen = 0;
         for(char a : S.toCharArray()){
             if(isDigit(a)){
-                String tempBuffer = buffer;
-                for(int i = 0; i < a-'0'-1; i++){
-                    buffer += tempBuffer;
-                    if(buffer.length() >= K){
-                        return buffer.substring(K-1,K);
-                    }
-                }
+                decodedLen *= a - '0';
             }
             else{
-                buffer += a;
-                if(buffer.length() >= K){
-                    return buffer.substring(K-1,K);
-                }
+                decodedLen++;
             }
         }
-        while(buffer.length() < K){
-            buffer += buffer;
+        long K = k;
+        for(char a : new StringBuilder(S).reverse().toString().toCharArray()){
+            if(isDigit(a)){
+                decodedLen /= a-'0';
+                K = K % decodedLen;
+            }
+            else{
+                if(K % decodedLen == 0){
+                    return "" + a;
+                }
+                decodedLen--;
+            }
         }
-        return buffer.substring(K-1,K);
+        return "";
     }
 
     private boolean isDigit(char a){
@@ -38,6 +39,7 @@ class Solution {
 
     public static void main(String args[]){
         Solution s = new Solution();
+        System.out.println(s.decodeAtIndex("leet2c12345", 36));
         System.out.println(s.decodeAtIndex("y959q969u3hb22odq595", 222280369));
     }
 }
