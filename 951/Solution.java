dac9f6558f251f0dd22acb8fddee692b7f8387d6
Ming Ki Chong <unexpected.exception@gmail.com>
2019-10-11 01:19:26 -0700

    https://leetcode.com/problems/flip-equivalent-binary-trees/ (refactored)

--- 951/Solution.java
+++ 951/Solution.java
@@ -8,13 +8,8 @@ class Solution {
         else if(root1 == null || root2 == null || (root1.val != root2.val)){
             return false;
         }
-        if(root1.left != null && root2.left != null && root1.left.val == root2.left.val ||
-           root1.right != null && root2.right != null && root1.right.val == root2.right.val){
-            return flipEquiv(root1.left, root2.left) && flipEquiv(root1.right, root2.right);
-        }
-        else{
-            return flipEquiv(root1.right, root2.left) && flipEquiv(root1.left, root2.right);
-        }
+        return flipEquiv(root1.left, root2.left)  && flipEquiv(root1.right, root2.right) ||
+               flipEquiv(root1.right, root2.left) && flipEquiv(root1.left, root2.right);
     }
 
     public static void main(String args[]){
@@ -22,6 +17,11 @@ class Solution {
         TreeNode root1 = TreeNode.createBinaryTree(new Integer[]{0, null, 1});
         TreeNode root2 = TreeNode.createBinaryTree(new Integer[]{0, null, 1});
         System.out.println(s.flipEquiv(root1, root2));
+
+        root1 = TreeNode.createBinaryTree(new Integer[]{1,2,3,4,5,6,null,null,null,7,8});
+        root2 = TreeNode.createBinaryTree(new Integer[]{1,3,2,null,6,4,5,null,null,null,null,8,7});
+        System.out.println(s.flipEquiv(root1, root2));
+
     }
 }
 
