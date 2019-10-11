5555e504d0d20f7c9577461c595a6e319d0cf9e2
Ming Ki Chong <unexpected.exception@gmail.com>
2019-10-10 23:08:45 -0700

    https://leetcode.com/problems/maximum-level-sum-of-a-binary-tree/ (DFS)

--- 1161/Solution.java
+++ 1161/Solution.java
@@ -1,6 +1,35 @@
 import java.util.*;
 
 class Solution {
+    public int maxLevelSumDFS(TreeNode root) {
+        ArrayList<Integer> levelSums = new ArrayList<>();
+        dfs(root, 1, levelSums);
+        int max = Integer.MIN_VALUE;
+        int maxLevel = 0;
+        for(int i = 0; i < levelSums.size(); i++){
+            if(max < levelSums.get(i)){
+                max = levelSums.get(i);
+                maxLevel = i;
+            }
+        }
+        return maxLevel + 1;
+    }
+
+    private void dfs(TreeNode node, int depth, ArrayList<Integer> levelSums){
+        if(levelSums.size() < depth){
+            levelSums.add(node.val);
+        }
+        else{
+            levelSums.set(depth-1, levelSums.get(depth-1) + node.val);
+        }
+        if(node.left != null){
+            dfs(node.left, depth + 1, levelSums);
+        }
+        if(node.right != null){
+            dfs(node.right, depth + 1, levelSums);
+        }
+    }
+
     public int maxLevelSum(TreeNode root) {
         ArrayList<Integer> levelSums = new ArrayList<>();
 
@@ -46,6 +75,7 @@ class Solution {
         Solution s = new Solution();
         TreeNode root = TreeNode.createBinaryTree(new Integer[]{989,null,10250,98693,-89388,null,null,null,-32127});
         System.out.println(s.maxLevelSum(root));
+        System.out.println(s.maxLevelSumDFS(root));
     }
 }
 
