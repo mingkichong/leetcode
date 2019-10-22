d3a79fa57a458e5ab9da8b776941fc964cde0a9a
Ming Ki Chong <unexpected.exception@gmail.com>
2019-10-22 16:43:06 -0700

    https://leetcode.com/problems/minimum-absolute-difference/

---
+++ 1200/Solution.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution {
+
+    public List<List<Integer>> minimumAbsDifference(int[] arr) {
+        Arrays.sort(arr);
+        int minDiff = Integer.MAX_VALUE;
+        for(int i = 0; i < arr.length - 1; i++){
+            minDiff = Math.min(minDiff, Math.abs(arr[i] - arr[i+1]));
+        }
+        ArrayList<List<Integer>> results = new ArrayList<>();
+        for(int i = 0; i < arr.length - 1; i++){
+            if(Math.abs(arr[i] - arr[i+1]) == minDiff){
+                ArrayList<Integer> list = new ArrayList<>();
+                list.add(arr[i]);
+                list.add(arr[i+1]);
+                results.add(list);
+            }
+        }
+        return results;
+    }
+
+    public static void main(String args[]){
+        Solution s = new Solution();
+        int [] arr = {3,8,-10,23,19,-4,-14,27};
+        System.out.println(s.minimumAbsDifference(arr));
+    }
+}
+
