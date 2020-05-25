0962aeb0455bf1aaa8d8740a6c5a327cfae16303
Ming Ki Chong <unexpected.exception@gmail.com>
2020-05-25 02:11:48 -0700

    print tree horizontally

--- Templates/Tree/TreeNode.java
+++ Templates/Tree/TreeNode.java
@@ -20,7 +20,7 @@ public class TreeNode {
 
     @Override
     public String toString(){
-        return getTreeWithStructure(this);
+        return BTreePrinter.printNode(this);
     }
 
     public static TreeNode createBinarySearchTree(int [][] nums){
@@ -229,6 +229,122 @@ public class TreeNode {
                 queue.add(node.left);
                 queue.add(node.right);
             }
+        }
+    }
+
+    // https://www.inlumina.work/core-java/java-binary-tree-printer/
+    static class BTreePrinter {
+        private static StringBuilder sb = null;
+        public static <T extends Comparable<?>> String printNode(TreeNode root) {
+            sb = new StringBuilder();
+            int maxLevel = BTreePrinter.maxLevel(root);
+
+            printNodeInternal(Collections.singletonList(root), 1, maxLevel);
+            return sb.toString();
+        }
+
+        private static <T extends Comparable<?>> void printNodeInternal(List<TreeNode> nodes, int level, int maxLevel) {
+            if (nodes.isEmpty() || BTreePrinter.isAllElementsNull(nodes)) {
+                return;
+            }
+
+            int floor = maxLevel - level;
+            int endgeLines = (int) Math.pow(2, (Math.max(floor - 1, 0)));
+            int firstSpaces = (int) Math.pow(2, (floor)) - 1;
+            int betweenSpaces = (int) Math.pow(2, (floor + 1)) - 1;
+
+            BTreePrinter.printWhitespaces(firstSpaces);
+
+            List<TreeNode> newNodes = new ArrayList<TreeNode>();
+            for (TreeNode node : nodes) {
+                if (node != null) {
+                    print(node.val);
+                    newNodes.add(node.left);
+                    newNodes.add(node.right);
+                } else {
+                    newNodes.add(null);
+                    newNodes.add(null);
+                    print(" ");
+                }
+
+                BTreePrinter.printWhitespaces(betweenSpaces);
+            }
+            println();
+
+            for (int i = 1; i <= endgeLines; i++) {
+                for (int j = 0; j < nodes.size(); j++) {
+                    BTreePrinter.printWhitespaces(firstSpaces - i);
+                    if (nodes.get(j) == null) {
+                        BTreePrinter.printWhitespaces(endgeLines + endgeLines + i + 1);
+                        continue;
+                    }
+
+                    if (nodes.get(j).left != null) {
+                        print("/");
+                    } else {
+                        BTreePrinter.printWhitespaces(1);
+                    }
+
+                    BTreePrinter.printWhitespaces(i + i - 1);
+
+                    if (nodes.get(j).right != null) {
+                        print("\\");
+                    } else {
+                        BTreePrinter.printWhitespaces(1);
+                    }
+
+                    BTreePrinter.printWhitespaces(endgeLines + endgeLines - i);
+                }
+
+                println();
+            }
+
+            printNodeInternal(newNodes, level + 1, maxLevel);
+        }
+
+        private static void printWhitespaces(int count) {
+            for (int i = 0; i < count; i++) {
+                print(" ");
+            }
+        }
+
+        private static <T extends Comparable<?>> int maxLevel(TreeNode node) {
+            if (node == null) {
+                return 0;
+            }
+
+            return Math.max(BTreePrinter.maxLevel(node.left), BTreePrinter.maxLevel(node.right)) + 1;
+        }
+
+        private static <T> boolean isAllElementsNull(List<T> list) {
+            for (Object object : list) {
+                if (object != null) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private static void print(int val){
+            print(String.valueOf(val));
+        }
+
+        private static void print(String str){
+            sb.append(str);
+        }
+
+        private static void println(int val){
+            println(String.valueOf(val));
+        }
+
+        private static void println(String str){
+            print(str);
+            print("\n");
+        }
+
+        private static void println(){
+            println("");
         }
     }
 }
