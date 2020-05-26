2a987ad3029bfba95fbf7d3f98ce73def4f671da
Ming Ki Chong <unexpected.exception@gmail.com>
2020-05-26 00:36:24 -0700

    https://leetcode.com/problems/task-scheduler/
    HashMap for histogram

--- 621/Solution.java
+++ 621/Solution.java
@@ -6,14 +6,14 @@ class Solution {
         if (n == 0) {
             return tasks.length;
         }
-        int [] hist = new int [26];
+        HashMap<Character, Integer> hist = new HashMap<>();
         int max = 0;
         for (char a : tasks) {
-            hist[a - 'A']++;
-            max = Math.max(max, hist[a - 'A']);
+            hist.put(a, hist.getOrDefault(a, 0) + 1);
+            max = Math.max(max, hist.get(a));
         }
         int countMax = 0;
-        for (int m : hist) {
+        for (int m : hist.values()) {
             countMax += m == max ? 1 : 0;
         }
         int num = max + (max - 1) * n + countMax - 1;
@@ -22,10 +22,10 @@ class Solution {
 
     public static void main(String args[]) {
         Solution s = new Solution();
-        System.out.println(s.leastInterval(new char[] {'A', 'A', 'A', 'B', 'B', 'B'}, 2));
-        System.out.println(s.leastInterval(new char[] {'A', 'A', 'A', 'B', 'B', 'B'}, 0));
-        System.out.println(s.leastInterval(new char[] {'A', 'A', 'A'}, 2));
-        System.out.println(s.leastInterval(new char[] {'A', 'A', 'A', 'A', 'A', 'B', 'C', 'D', 'E', 'F', 'G'}, 1));
+        System.out.println(s.leastInterval(new char[] {'A', 'A', 'A', 'B', 'B', 'B'}, 2)); // 8
+        System.out.println(s.leastInterval(new char[] {'A', 'A', 'A', 'B', 'B', 'B'}, 0)); // 6
+        System.out.println(s.leastInterval(new char[] {'A', 'A', 'A'}, 2)); // 7
+        System.out.println(s.leastInterval(new char[] {'A', 'A', 'A', 'A', 'A', 'B', 'C', 'D', 'E', 'F', 'G'}, 1)); // 11
     }
 }
 
