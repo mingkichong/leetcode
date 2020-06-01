5a2120ccb7aae4cd63796038def4760e6582ecbe
Ming Ki Chong <unexpected.exception@gmail.com>
2020-05-31 23:25:15 -0700

    https://leetcode.com/problems/subtree-of-another-tree/

---
+++ 572/Solution.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+class Solution {
+
+    public boolean isSubtree(TreeNode s, TreeNode t) {
+        if (s == null && t == null) {
+            return true;
+        } else if (s == null && t != null) {
+            return false;
+        }
+        if (isIdentical(s, t)) {
+            return true;
+        }
+        return isSubtree(s.left, t) || isSubtree(s.right, t);
+    }
+
+    private boolean isIdentical(TreeNode s, TreeNode t) {
+        if (s == null && t == null) {
+            return true;
+        } else if (s == null && t != null || s != null && t == null || s.val != t.val) {
+            return false;
+        }
+        return isIdentical(s.left, t.left) && isIdentical(s.right, t.right);
+    }
+
+    public static void main(String args[]) {
+        Solution s = new Solution();
+        TreeNode sNode = null, tNode = null;
+
+        sNode = TreeNode.createBinaryTree(new Integer[] {3, 4, 5, 1, 2});
+        tNode = TreeNode.createBinaryTree(new Integer[] {4, 1, 2});
+        System.out.println(s.isSubtree(sNode, tNode)); // true
+
+        sNode = TreeNode.createBinaryTree(new Integer[] {3, 4, 5, 1, 2, null, null, null, null, 0});
+        tNode = TreeNode.createBinaryTree(new Integer[] {4, 1, 2});
+        System.out.println(s.isSubtree(sNode, tNode)); // false
+
+        sNode = TreeNode.createBinaryTree(new Integer[] {1, 1});
+        tNode = TreeNode.createBinaryTree(new Integer[] {1});
+        System.out.println(s.isSubtree(sNode, tNode)); // true
+
+        sNode = TreeNode.createBinaryTree(new Integer[] {3, 4, 5, 1, null, 2});
+        tNode = TreeNode.createBinaryTree(new Integer[] {3, 1, 2});
+        System.out.println(s.isSubtree(sNode, tNode)); // false
+    }
+}
+
