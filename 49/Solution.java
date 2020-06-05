a70ceb2af1374f0f1d6006a80d665da8494fba9c
Ming Ki Chong <unexpected.exception@gmail.com>
2020-06-04 19:43:32 -0700

    https://leetcode.com/problems/group-anagrams/

--- 49/Solution.java
+++ 49/Solution.java
@@ -2,44 +2,25 @@ import java.util.*;
 
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        HashMap<String, List<String>> map = new HashMap<String, List<String>>();
-        for(String str : strs){
+        HashMap<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
             String key = getKey(str);
-            List<String> keyStrings = map.getOrDefault(key, new ArrayList<String>());
-            keyStrings.add(str);
-            map.put(key, keyStrings);
+            if (!map.containsKey(key)) {
+                map.put(key, new ArrayList<>());
+            }
+            map.get(key).add(str);
         }
-        List<List<String>> results = new ArrayList<List<String>>();
-        for(List<String> entry : map.values()){
-            results.add(entry);
-        }
-        return results;
+        return new ArrayList<>(map.values());
     }
 
-    String getKey(String str){
+    String getKey(String str) {
         char [] a = str.toCharArray();
         Arrays.sort(a);
         return new String(a);
     }
 
-    public static void main(String args[]){
+    public static void main(String args[]) {
         Solution s = new Solution();
-
-        String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
-        List<List<String>> groups = s.groupAnagrams(strs);
-        if(groups == null){
-            return;
-        }
-        StringBuilder sb = new StringBuilder();
-        for(List<String> group : groups){
-            for(String str : group){
-                sb.append(str);
-                sb.append(", ");
-            }
-            sb.delete(sb.length() - 2, sb.length());
-            sb.append("\n");
-        }
-        sb.delete(sb.length() - 1, sb.length());
-        System.out.println(sb.toString());
+        System.out.println(s.groupAnagrams(new String[] {"eat", "tea", "tan", "ate", "nat", "bat"}));
     }
 }
